ea0747fb8853b81280ad78aac1e6c26b009c2dae
Vladyslav Tomashpolskyi <wizzardich@gmail.com>
2015-09-15 20:40:08 +0200

    Implemented Population interface.

--- src/org/rug/masdesign/agents/Population.java
+++ src/org/rug/masdesign/agents/Population.java
@@ -4,4 +4,14 @@ package org.rug.masdesign.agents;
  * Created by wizzardich on 9/15/15.
  */
 public class Population {
+    private int groupSize;
+    private double startingGossipProbability;
+
+    public void nextRound(){
+
+    }
+
+    public void nextGeneration() {
+
+    }
 }
